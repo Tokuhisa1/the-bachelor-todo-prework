87e8fe1913f4f4877d6a7e17241ebd3005706a91
Winston Tokuhisa <tokuhisawinston@outlook.com>
2019-05-23 19:48:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -50,7 +50,7 @@ def get_average_age_for_season(data, season)
   count = 0
 
   data[season.to_sym].each do |key, value|
-   ages = ages + key[:age].to_f
+   ages = ages + key['age'].to_f
    count = count + 1
   end
 
